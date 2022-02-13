2af98a607f90b0eebf0e99e90dd0af9ca73398aa
Max Shaptala <maxshaptala@gmail.com>
2022-02-13 14:50:03 +0200

    Post Request Handling

--- src/main/java/org/itstep/controller/HomeController.java
+++ src/main/java/org/itstep/controller/HomeController.java
@@ -2,18 +2,33 @@ package org.itstep.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(path = "/")
+    List<String> tasks = new CopyOnWriteArrayList<>();
+
+//    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @GetMapping(path = "/")
     String index(Model model) {
         model.addAttribute("title", "Home page");
         model.addAttribute("time", LocalDateTime.now());
+        model.addAttribute("tasks", tasks);
         return "index";
+    }
+
+    @PostMapping(path="/todo")
+    String save(String task) {
+        if(!task.isBlank()) {
+            tasks.add(task);
+        }
+        return "redirect:/";
     }
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,34 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Title</title>
+    <link href="/resources/css/style.css" rel="stylesheet"/>
 </head>
 <body>
-    <h1>${title}</h1>
+<h1>${title}</h1>
+<form method="post" action="/todo">
+    <label>
+        Task: <input name="task">
+    </label>
+    <button type="submit">Add</button>
+</form>
+<table>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Task</th>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach items="${tasks}" var="task" varStatus="status">
+        <tr>
+            <td>${status.count}</td>
+            <td>${task}</td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
 <footer>
     <p>Current time: ${time}</p>
 </footer>
